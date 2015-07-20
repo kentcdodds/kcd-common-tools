d88a784e81de786cc50a6e34a2944c6be24bc488
Kent C. Dodds <kent+github@doddsfamily.us>
2015-07-20 09:16:39 -0600

    1.0.0-beta.15

--- utils/install.sh
+++ utils/install.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Installing kcd-common-tools"
 
-ln -s ./shared/link/editorconfig ../.editorconfig
-ln -s ./shared/link/eslintignore ../.eslintignore
-ln -s ./shared/link/gitignore ../.gitignore
-ln -s ./shared/link/npmignore ../.npmignore
+ln -s ./shared/link/editorconfig .editorconfig
+ln -s ./shared/link/eslintignore .eslintignore
+ln -s ./shared/link/gitignore .gitignore
+ln -s ./shared/link/npmignore .npmignore
 
-cp ./shared/copy/* ../../../.
+cp ./shared/copy/* ../../.
 
 echo "Things linked and copied."
 echo "You may want to run this to install some deps for your project:"
